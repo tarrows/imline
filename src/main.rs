94b3bdec711d34fe58e05255164f3c3b880d7bf6
c10t <c10t@fa.ke>
2020-01-13 23:27:03 +0900

    read dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+// extern crate image;
+use std::fs;
+use std::io;
+
+fn main() -> io::Result<()> {
+  let mut entries = fs::read_dir(".")?
+    .map(|res| res.map(|e| e.path()))
+    .collect::<Result<Vec<_>, io::Error>>()?;
+
+  println!("{:?}", entries);
+
+  Ok(())
 }
